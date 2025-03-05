4a4273d43d8fea172e0c7d4107a83382dc88a48c
Daniel Sędłak <109912419+DanieloSendi@users.noreply.github.com>
2025-03-05 13:45:18 +0000

    Added CRUD functionality for expenses

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import home
+from .views import expense_list, expense_create, expense_update, expense_delete
 
 urlpatterns = [
     path("", home, name="expenses-home"),
+    path("", expense_list, name="expense-list"),
+    path("add/", expense_create, name="expense-create"),
+    path("edit/<int:pk>/", expense_update, name="expense-update"),
+    path("delete/<int:pk>/", expense_delete, name="expense-delete"),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -2,7 +2,52 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.contrib.auth.decorators import login_required
+from .models import Expense
+from django.shortcuts import get_object_or_404, redirect
+from .forms import ExpenseForm
 
 @login_required
 def home(request):
     return render(request, "expenses/home.html")
+
+# Takes all the expenses of a logged-in user and sorts them descending by date.
+@login_required
+def expense_list(request):
+    expenses = Expense.objects.filter(user=request.user).order_by("-date")
+    return render(request, "expenses/expense_list.html", {"expenses": expenses})
+
+# Creates a new expense and assigns it to the currently logged-in user.
+@login_required
+def expense_create(request):
+    if request.method == "POST":
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            expense = form.save(commit=False)
+            expense.user = request.user
+            expense.save()
+            return redirect("expense-list")
+    else:
+        form = ExpenseForm()
+    return render(request, "expenses/expense_form.html", {"form": form})
+
+# It retrieves the expense from the database and allows you to edit it.
+@login_required
+def expense_update(request, pk):
+    expense = get_object_or_404(Expense, pk=pk, user=request.user)
+    if request.method == "POST":
+        form = ExpenseForm(request.POST, instance=expense)
+        if form.is_valid():
+            form.save()
+            return redirect("expense-list")
+    else:
+        form = ExpenseForm(instance=expense)
+    return render(request, "expenses/expense_form.html", {"form": form})
+
+# Deletes the expense only if the user confirms it.
+@login_required
+def expense_delete(request, pk):
+    expense = get_object_or_404(Expense, pk=pk, user=request.user)
+    if request.method == "POST":
+        expense.delete()
+        return redirect("expense-list")
+    return render(request, "expenses/expense_confirm_delete.html", {"expense": expense})
