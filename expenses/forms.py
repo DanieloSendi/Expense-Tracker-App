670c29d1b308e4be1d62d450eaa3335d550fba4c
Daniel Sędłak <109912419+DanieloSendi@users.noreply.github.com>
2025-03-06 19:36:47 +0000

    Added date picker in expense form

--- expenses/forms.py
+++ expenses/forms.py
@@ -2,6 +2,11 @@ from django import forms
 from .models import Expense
 
 class ExpenseForm(forms.ModelForm):
+    date = forms.DateField(
+        widget=forms.DateInput(format="%Y-%m-%d", attrs={"type": "date"}),  
+        input_formats=["%Y-%m-%d"]
+    )
+    
     class Meta:
         model = Expense
         fields = ['category', 'amount', 'date', 'description']
