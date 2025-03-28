62a2cc5753d374d65472996d169a8fabb2509d69
DanieloSendi <danielosedlak@gmail.com>
2025-03-29 00:18:24 +0100

    Improved users form UX: placeholders, required, Bootstrap classes

--- users/forms.py
+++ users/forms.py
@@ -4,17 +4,54 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Enter password',
+            'class': 'form-control',
+            'required': True,
+        })
+    )
+    password2 = forms.CharField(
+        label="Confirm Password",
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat password',
+            'class': 'form-control',
+            'required': True,
+        })
+    )
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'placeholder': 'Type a username',
+                'class': 'form-control',
+                'required': True,
+            }),
+            'email': forms.EmailInput(attrs={
+                'placeholder': 'Enter email (eg. username@mail.com)',
+                'class': 'form-control',
+                'required': True,
+            }),
+        }
 
 
 class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Update email address',
+        'class': 'form-control',
+        'required': True,
+    }))
 
     class Meta:
         model = User
         fields = ["username", "email"]
-        
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'placeholder': 'Update username',
+                'class': 'form-control',
+                'required': True,
+            }),
+        }
